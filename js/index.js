77ecc881107d3b8126637f790f889c930b392a26
vipin-frontend-developer <vipin.frontend@gmail.com>
2022-09-03 10:35:16 -0400

    jsfilemodifies.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
+
 burger=document.querySelector('.burger')
 listitem=document.querySelector('.listitem')
 rightnav=document.querySelector('.rightnav')
 navbar=document.querySelector('.navbar')
 
 burger.addEventListener('click',()=>{
-    navbar.classList.toggle('nav-h-resp');
+    
     listitem.classList.toggle('v-class-resp');
     rightnav.classList.toggle('v-class-resp');
-})
+    navbar.classList.toggle('nav-h-resp');
+});
+
+
+
